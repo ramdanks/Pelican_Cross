8830cc27454c5bb2280698bcb53e03d8f07f3980
ramdanks <51152251+ramdanks@users.noreply.github.com>
2020-04-29 18:32:11 +0700

    Add files via upload

---
+++ Source.vhd
@@ -0,0 +1,31 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_unsigned.all;
+use ieee.numeric_std.all;
+
+entity PELICAN is
+	port
+	(
+		--Input Ports
+		pButton	: in std_logic;
+		pClk	: in std_logic;
+		--Output Ports
+		pAlarm	: out std_logic;
+		pSevseg	: out std_logic_vector( 6 downto 0 );
+		pYellow	: out std_logic;
+		pRed	: out std_logic
+	);
+end PELICAN;
+
+architecture behaviour of PELICAN is
+
+	type LightState is (IDLE, STOP, COUNTDOWN);
+	
+	signal state 		: lampuState := IDLE;
+	signal nextState 	: lampuState := IDLE; 
+
+begin
+
+
+
+end architecture;
