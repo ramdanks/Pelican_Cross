f2630dc95c8426e29c1d4fa59ec64e6e460bb11c
ramdanks <ramdanks@users.noreply.github.com>
2020-05-01 21:17:03 +0700

    Clean Up Codes
    Fix Seven segment delay, code optimization, pLCD is set to give signal to pedestrian.

--- Source.vhd
+++ Source.vhd
@@ -8,13 +8,13 @@ entity PELICAN is
 	(
 		--Input Ports
 		pButton	: in std_logic;
-		pClk	   : in std_logic;
+		pClk	: in std_logic;
 		--Output Ports
-		pAlarm	: out std_logic;
-		pSevseg	: inout std_logic_vector( 7 downto 0 );
-		pYellow	: inout std_logic;
-		pLCD     : out string (1 to 5);
-		pRed	   : out std_logic
+		pAlarm	: out std_logic := '0';
+		pSevseg	: out std_logic_vector( 7 downto 0 ) := "11111111";
+		pLCD    : out string (1 to 5) := "STOP!";
+		pYellow	: inout std_logic := '1';
+		pRed	: out std_logic	:= '0'
 	);
 end PELICAN;
 
@@ -25,7 +25,6 @@ architecture behaviour of PELICAN is
 	signal state 		: LightState := IDLE;
 	signal nextState 	: LightState := IDLE;
 	signal counter		: integer := 9; 
-	signal dec 			: std_logic_vector(3 downto 0) := "1001";  
 	
 	constant pWalk : string (1 to 5) := "GO   "; 
 	constant pStop : string (1 to 5) := "STOP!";
@@ -57,6 +56,7 @@ Control : process(pClk,pButton,counter) is begin
 		if (state = IDLE) then
 		
 			nextState <= COUNTDOWN;
+			
 		end if;
 	
 	end if;
@@ -72,19 +72,23 @@ Light : process(pClk,pYellow,state) is begin
 			when IDLE  =>
 			
 				pYellow <= not pYellow;
-				pRed <= '0';
+				pRed    <= '0';
+				pAlarm  <= '0';
+				pLCD 	<= pStop;
 			
 			when STOP =>
 			
-				pRed <= '1';
+				pRed    <= '1';
 				pYellow <= '0';
-				pLCD    <= pStop; 
+				pLCD    <= pWalk;
+				pAlarm  <= '1';
 				
 			when COUNTDOWN =>
 			
 				pYellow <= '1';
-				pRed <= '0';
-				pLCD <= pWalk;
+				pRed    <= '0';
+				pLCD    <= pStop;
+				pAlarm  <= '0';
 		
 		end case;
 	
@@ -95,43 +99,32 @@ end process;
 procCounter : process(pClk,counter) is begin
 
 	if (rising_edge(pClk) and state /= IDLE) then
-		
-		if ( counter < 1 ) 	then	
-		counter <= 9;
-		dec     <= "1001"; 
-		else	
-      		
-		counter <= counter - 1;
-		dec     <= std_logic_vector(unsigned(dec) - 1); 
-		case dec is 
-			when "0000" =>
-			pSevseg <= "10111111"; ---0
-			when "0001" =>
-			pSevseg <= "10000110"; ---1
-			when "0010" =>
-			pSevseg <= "11011011"; ---2
-			when "0011" =>
-			pSevseg <= "11001111"; ---3
-			when "0100" =>
-			pSevseg <= "11100110"; ---4
-			when "0101" =>
-			pSevseg <= "11101101"; ---5
-			when "0110" =>
-			pSevseg <= "11111100"; ---6
-			when "0111" =>
-			pSevseg <= "10000111"; ---7
-			when "1000" =>
-			pSevseg <= "11111111"; ---8
-			when "1001" =>
-			pSevseg <= "11101111"; ---9
-			when others =>
-			pSevseg <= "10110111"; ---null
-			end case;
-		
+		if ( counter < 1 ) 	then	counter <= 9;
+		else						counter <= counter - 1;
 		end if;
-	
 	end if;
 
+end process;
+
+SegmentProc : process(state,counter) is begin
+
+	if (state /= IDLE) then
+		case counter is 
+			when 0 => 		pSevseg <= "10111111"; --0
+			when 1 => 		pSevseg <= "10000110"; --1
+			when 2 => 		pSevseg <= "11011011"; --2
+			when 3 => 		pSevseg <= "11001111"; --3
+			when 4 => 		pSevseg <= "11100110"; --4
+			when 5 => 		pSevseg <= "11101101"; --5
+			when 6 => 		pSevseg <= "11111100"; --6
+			when 7 => 		pSevseg <= "10000111"; --7
+			when 8 => 		pSevseg <= "11111111"; --8
+			when 9 => 		pSevseg <= "11101111"; --9
+			when others =>	pSevseg <= "11111111";--Shut
+		end case;
+	else					pSevseg <= "11111111";--Shut
+	end if;
+	
 end process;
 
 end architecture;
